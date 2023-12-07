b0334b719b57c1a487f62e426e59b08c13fe9a4c
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2023-12-07 11:48:11 +0530

    Add files via upload

---
+++ num pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,k,l,m;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(k=i; k<5; k++)
+		{
+			printf(" ");
+		}
+		for(j=1; j<=i; j++)
+		{
+			printf("%d",j);
+		}
+		for(m=i-1; m>=1; m--)
+		{
+			printf("%d",m);
+		}
+		printf("\n");
+	}
+}

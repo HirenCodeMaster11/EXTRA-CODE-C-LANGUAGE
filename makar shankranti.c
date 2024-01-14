548afb74a9098edc003f9b36de967d38e1b91843
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2024-01-14 21:15:46 +0530

    Add files via upload

---
+++ makar shankranti.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\t\t\t\t\t\tHappy Makar Sankranti\n\n");
+	
+	printf("\t\t\t\t\t\tMakar Sankranti Wishes \n\n");
+	printf("Makar Sankranti, also known as the harvest festival, symbolizes the Sun's transition into the Capricorn constellation, marking the onset of longer days. The festival holds particular importance for farming communities as it signifies the culmination of their hard labor and brings joy, abundance, and celebration. Makar Sankranti in 2024 will be celebrated on January 15th. Rooted in tradition, Makar Sankranti reflects the spirit of the agricultural landscape.");
+}

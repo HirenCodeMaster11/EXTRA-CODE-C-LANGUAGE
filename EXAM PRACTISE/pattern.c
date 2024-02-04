32bfed8c809ef14a3dd569c51dd3a2bcc525f8a5
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2024-02-04 15:45:34 +0530

    Add files via upload

---
+++ EXAM PRACTISE/pattern.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+//1 2 3 4 5
+// 2 3 4 5
+//  3 4 5
+//   4 5
+//    5
+//   4 5
+//  3 4 5
+// 2 3 4 5
+//1 2 3 4 5
+main()
+{
+	int i,j,k;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(k=1; k<i; k++)
+		{
+			printf(" ");
+		}
+		for(j=i; j<=5; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	for(i=4; i>=1; i--)
+	{
+		for(k=1; k<i; k++)
+		{
+			printf(" ");
+		}
+		for(j=i; j<=5; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
